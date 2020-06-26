e2f2e9ea521938f32c47654e0196d10f73370817
Lorna Kamau <kamaulorna@gmail.com>
2020-06-26 13:02:11 +0300

    create image model and it's attributes

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image= models.ImageField(upload_to = 'images/')
+    description = models.CharField(max_length = 40)
+    name= models.CharField(max_length = 30)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
