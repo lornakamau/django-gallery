70d770ea4088de644cbedac7868d18d02a3f32ac
Lorna Kamau <kamaulorna@gmail.com>
2020-06-26 13:03:11 +0300

    add image model methods

--- gallery/models.py
+++ gallery/models.py
@@ -11,3 +11,33 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls,id,image):
+        cls.objects.filter(id=id).update(image=image)
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def search_image(cls,category):
+        try:
+            searched = Category.objects.get(name = category)
+            images = Image.objects.filter(category = searched.id)
+            return images
+        except Exception:
+            return  "No images were found for that category"
+
+    @classmethod
+    def filter_by_location(cls,location):
+        searched = Location.objects.get(name = location)
+        images = Image.objects.filter(location = searched.id)
+        return images 
