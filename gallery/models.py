5ee9cffc8d9dd980b39f66a867c0cb65a30cc7a7
Lorna Kamau <kamaulorna@gmail.com>
2020-06-26 13:06:55 +0300

    create category model and model methods

--- gallery/models.py
+++ gallery/models.py
@@ -57,3 +57,23 @@ class Category(models.Model):
     @classmethod
     def update_category(cls,id,name):
         cls.objects.filter(id = id).update(name = name)
+
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def update_location(cls,id,name):
+        cls.objects.filter(id = id).update(name = name)
+
+    @classmethod
+    def display_all_locations(cls):
+        return cls.objects.all()
