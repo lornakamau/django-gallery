d5f93dc7b2e65b5c94d3a12b9c6474e015c5fa70
Lorna Kamau <kamaulorna@gmail.com>
2020-06-26 13:43:03 +0300

    add location model tests

--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls,id,name):
+        cls.objects.filter(id = id).update(name = name)
+
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def update_location(cls,id,name):
+        cls.objects.filter(id = id).update(name = name)
+
+    @classmethod
+    def display_all_locations(cls):
+        return cls.objects.all()
+
 class Image(models.Model):
     image= models.ImageField(upload_to = 'images/')
     description = models.CharField(max_length = 40)
@@ -41,39 +76,3 @@ class Image(models.Model):
         searched = Location.objects.get(name = location)
         images = Image.objects.filter(location = searched.id)
         return images 
-
-class Category(models.Model):
-    name = models.CharField(max_length = 30)
-
-    def __str__(self):
-        return self.name
-
-    def save_category(self):
-        self.save()
-
-    def delete_category(self):
-        self.delete()
-
-    @classmethod
-    def update_category(cls,id,name):
-        cls.objects.filter(id = id).update(name = name)
-
-class Location(models.Model):
-    name = models.CharField(max_length = 30)
-
-    def __str__(self):
-        return self.name
-
-    def save_location(self):
-        self.save()
-
-    def delete_location(self):
-        self.delete()
-
-    @classmethod
-    def update_location(cls,id,name):
-        cls.objects.filter(id = id).update(name = name)
-
-    @classmethod
-    def display_all_locations(cls):
-        return cls.objects.all()
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from .models import Location, Category, Image
 
-# Create your tests here.
+class LocationTest(TestCase):
+    def setUp(self):
+        '''
+        Create new instances before a test
+        '''
+        self.nairobi = Location(name = "Nairobi")
+        self.tokyo = Location(name = "Tokyo")
+
+    def tearDown(self):
+        '''
+        Clear database after each test
+        '''
+        Location.objects.all().delete()
+
+    def test_location_instance(self):
+        '''
+        test whether the new location created is an instance of the Location class
+        '''
+        self.assertTrue(isinstance(self.nairobi, Location))
+    
+    def test_save_location(self):
+        '''
+        test whether new locations are added to the db 
+        '''
+        self.nairobi.save_location()
+        self.tokyo.save_location()
+        locations = Location.objects.all()
+        self.assertEqual(len(locations),2)
+
+    def test_delete_location(self):
+        '''
+        test whether a location is deleted
+        '''
+        self.nairobi.save_location()
+        locations1= Location.objects.all()
+        self.assertEqual(len(locations1),1)
+        self.nairobi.delete_location()
+        locations2= Location.objects.all()
+        self.assertEqual(len(locations2),0)
+
+    def test_update_location(self):
+        '''
+        test whether the location name is updated
+        '''
+        self.nairobi.save_location()
+        self.nairobi.update_location(self.nairobi.id,'Helsinki')
+        update = Location.objects.get(name = "Helsinki")
+        self.assertEqual(update.name, 'Helsinki')
+
+    def test_display_locations(self):
+        '''
+        This tests whether the display location function is getting the locations from the db
+        '''
+        self.nairobi.save_location()
+        self.tokyo.save_location()
+        self.assertEqual(len(Location.display_all_locations()), 2)
+    
