5537550b8dbec34317166487520930d9514e32f4
Lorna Kamau <kamaulorna@gmail.com>
2020-06-27 14:05:35 +0300

    add search view function and template

--- gallery/views.py
+++ gallery/views.py
@@ -5,3 +5,16 @@ from .models import Image
 def home(request):
     images = Image.objects.all()
     return render(request, 'home.html', {"images": images})
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        images = Image.search_image(category)
+        message = f"{category}"
+
+        return render(request, 'search.html',{"message":message,"images": images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
