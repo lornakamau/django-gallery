34dd5de0de7606e3f5d03e55b7fcb6977b3685ff
Lorna Kamau <kamaulorna@gmail.com>
2020-06-27 19:33:13 +0300

    add copy function and show modal function

--- static/js/base.js
+++ static/js/base.js
@@ -69,3 +69,21 @@ function openCard($card) {
     });
     
 }
+
+function showModal(name,desc,url, loc,cat){
+  $("#img-name").text(name)
+  $("#imageModal").modal("show")
+  $(".modal-title").text(name)
+  $(".mod-img").attr("src",url)
+  $("#img-desc").text(desc)
+  $("#img-loc").text("Location: " + loc)
+  $("#img-cat").text("Category: " + cat)
+  $("#copy-url").val(window.location.origin + url)
+}
+
+function copy(){
+  $("#copy-url").select()
+  document.execCommand('copy');
+  alert("Link has been copied")
+}
+
